1c10e107343ee391e5f20996fff1d5f91b5a5968
dkyzym <kyzym12345@gmail.com>
2024-07-29 12:30:23 +0200

    feat: add id to fast search results

--- controllers/data/ugFastSearchDataController.js
+++ controllers/data/ugFastSearchDataController.js
@@ -1,6 +1,7 @@
 import { fastSearchUGservice } from '#services/data/ugFastSearchService.js';
 import { checkEmptyField } from '#utils/validationHelpers.js';
 import { ctrlWrapper } from '#middlewares/ctrlWrapper.js';
+import { v4 as uuidv4 } from 'uuid';
 
 export const fastSearchUG = ctrlWrapper(async (req, res) => {
   const { term, locale } = req.query;
@@ -10,5 +11,9 @@ export const fastSearchUG = ctrlWrapper(async (req, res) => {
   const cookies = JSON.parse(req.cookies.ugCookies || '[]');
   const data = await fastSearchUGservice(term, locale, cookies);
 
-  res.json({ success: true, data });
+  const dataWithId = data.map((element) => {
+    return { id: uuidv4(), ...element };
+  });
+
+  res.json({ success: true, data: dataWithId });
 });
